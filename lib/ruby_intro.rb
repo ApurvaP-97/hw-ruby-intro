14bf53136ef215b4bac7fe5ceaaa932a4e6e229e
EC2 Default User <ec2-user@ip-172-31-30-195.ec2.internal>
2022-02-10 19:38:39 +0000

    Part1 and Part2 solved

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,62 @@
 
 # Part 1
 
-def sum arr
+def sum array
   # YOUR CODE HERE
+  array.reduce(0) { |sum, ele| sum + ele }
 end
 
-def max_2_sum arr
+def max_2_sum array
   # YOUR CODE HERE
+  array.sort!
+  sum(array.last(2))
 end
 
-def sum_to_n? arr, n
+def sum_to_n? array, n
   # YOUR CODE HERE
+  array.combination(2).any?{|a,b| a+b==n}
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  return false if s.empty?
+  pattern = /^[aeiouAEIOU|^\W].*/
+  !pattern.match(s[0])
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if (s) == "0"
+    return true
+  end
+  if /^[01]*(00)$/.match(s) # Multiple of 4 in binary will be ending with 00
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+# # YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError unless price.positive?
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    format("$%0.2f", @price)
+  end
 end
